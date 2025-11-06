4839a5fedb22eafac7105f1ada79cc0c131b7ea2
kemzsitink <hocctiengchokeu@gmail.com>
2025-11-07 00:09:35 +0700

    Fix curl URL in rjt-clisetup.sh

--- rjt-clisetup.sh
+++ rjt-clisetup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests psutil loguru prettytable colorama aiohttp
-curl -Ls "https://raw.githubusercontent.com/kemzsitink/rejointool-cli/refs/heads/main/rejointool-cli.py" -o /sdcard/Download/Rejoin.py
+curl -Ls "https://raw.githubusercontent.com/kemzsitink/rejointool-cli/main/rejointool-cli.py" -o /sdcard/Download/Rejoin.py
